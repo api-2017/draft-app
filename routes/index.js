fdc43db0d06dc5a553ef1657ee0fed6144495f16
Karl Stolley <karl.stolley@gmail.com>
2017-11-08 18:09:48 -0600

    Initial stab at inserting request code into route; halfway there

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+const request = require('request');
+const payload = {
+  url: 'https://api.github.com/user',
+  headers: {
+    'User-Agent': 'request',
+    'Authorization': `token ${process.env['GITHUB_USER_READ_TOKEN']}`
+  }
+}
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var a = {};
+  request(payload, (error, response, data) => {
+    // console.log('error:', error); // Print the error if one occurred
+    // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    //console.log('data:', data); // Print the data from the GitHub API
+    a = JSON.parse(data);
+    console.log(a.blog);
+    return a;
+  });
+  res.render('index', { title: a.blog });
 });
 
 module.exports = router;
