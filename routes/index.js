4e5a05b3e0d85fbe96ed2b7d0c5d26e46e253027
Karl Stolley <karl.stolley@gmail.com>
2017-11-13 17:59:04 -0600

    Simplify request and response for default index

--- routes/index.js
+++ routes/index.js
@@ -12,16 +12,18 @@ const payload = {
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var a = {};
   request(payload, (error, response, data) => {
     // console.log('error:', error); // Print the error if one occurred
     // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
     //console.log('data:', data); // Print the data from the GitHub API
-    a = JSON.parse(data);
+    var a = JSON.parse(data);
     console.log(a.blog);
-    return a;
+    res.render('index',
+    {
+      title: "GitHub Profile for " + a.name,
+      profile: a
+    });
   });
-  res.render('index', { title: a.blog });
 });
 
 module.exports = router;
